d656f000f6a3fb470c3ca293ee2efb4a060fa83d
Christian <christian@cachem.local>
2012-10-04 16:23:21 +0200

    AAA

--- KeyBoardAccessoryView.h
+++ KeyBoardAccessoryView.h
@@ -1,25 +1,21 @@
 //
-//  KeyBoardAccessory.h
+//  KeyboardAccessoryView.h
 //  RunCalc
 //
-//  Created by Christian on 16/09/12.
+//  Created by mishanet on 03/05/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface KeyboardAccessoryView : UIView
-{
-    id <UITextInput> __unsafe_unretained delegate;
-    UIButton *bDone;
+@interface KeyboardAccessoryView : UIView {
+//    id <UITextInput> __unsafe_unretained delegate;
     UIControl *activeControl;
 }
 
-@property (nonatomic, unsafe_unretained) id <UITextInput> delegate;
-@property (nonatomic) IBOutlet UIButton *bDone;
+//@property (nonatomic, unsafe_unretained) id <UITextInput> delegate;
+//@property (nonatomic) UIButton *bDone;
 @property (nonatomic) UIControl *activeControl;
 
-- (IBAction)closeKeyboard:(id)sender;
-//
-
+-(IBAction)closeKeyboard:(id)sender;
 @end
